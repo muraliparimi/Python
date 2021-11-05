a3a7db72f99f5a99a90d13e19c76a62e8700ca63
muralikrishnaparimi <allabtdata@gmail.com>
2021-11-05 17:24:10 -0500

    More csv examples

---
+++ code/DealWithFiles/quote_wrapping.py
@@ -0,0 +1,20 @@
+import csv
+import os
+
+data_loc='/Users/mp/programming/python3/github/Python/data'
+file_name='quote_wrapping.csv'
+
+file_path = os.path.join(data_loc, file_name)
+
+print(f''' Example to Read files with csv.reader function''')
+
+with open(file_path) as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter = ',',quotechar='"')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Coumn names are {", ".join(row)}')
+            line_count += 1
+        print(f'\t{row["name"]} lives at {row["address"]} and joined on {row["date joined"]}')
+        line_count += 1
+    print(f'Processed {line_count} lines.')
